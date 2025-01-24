ff4d10c790711e11f3337a70349df58e01f46f8f
CaptainCakalang <144084388+shvchenko@users.noreply.github.com>
2025-01-24 09:57:29 +0700

    Create greet.py
    A simple function to display text message

---
+++ greet.py
@@ -0,0 +1,11 @@
+# A simple feature: displaying a text message
+
+def display_message():
+    """
+    Function to display a text message.
+    """
+    message = "Welcome to our repository!"
+    print(message)
+
+if __name__ == "__main__":
+    display_message()
